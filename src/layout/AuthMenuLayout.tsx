f8a681a9c3de2004c21d7b2d707231d316fd6946
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2024-12-06 15:20:18 -0300

    drawer

--- src/layout/AuthMenuLayout.tsx
+++ src/layout/AuthMenuLayout.tsx
@@ -1,8 +1,15 @@
+import { Drawer } from "@mui/material";
 import { Outlet } from "react-router-dom";
 
 function AuthMenuLayout() {
   return (
     <main>
+      <Drawer
+        variant="temporary"
+        ModalProps={{
+          keepMounted: false,
+        }}
+      />
       <Outlet />
     </main>
   );
