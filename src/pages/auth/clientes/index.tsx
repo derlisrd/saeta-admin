60b68eecf2bbfc08f8526fa91345ea6425a4ba98
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2025-02-01 01:23:04 -0300

    clientes

--- src/pages/auth/clientes/index.tsx
+++ src/pages/auth/clientes/index.tsx
@@ -1,5 +1,14 @@
+import { Button, Container, Stack } from "@mui/material";
+
 function Clientes() {
-  return <h1>clientes</h1>;
+  return (
+    <Container sx={{ paddingBottom: 6 }}>
+      <Stack direction={{ xs: "row" }} justifyContent="space-between" alignItems="center" padding={2}>
+        <h3>Clientes</h3>
+        <Button onClick={() => {}}>Agregar nuevo</Button>
+      </Stack>
+    </Container>
+  );
 }
 
 export default Clientes;
