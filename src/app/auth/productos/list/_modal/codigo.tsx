a67bcbb9d7b0d41fb2d420aa8c2e174c1d99804c
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-04-30 17:31:44 -0300

    esperaaaa

--- src/app/auth/productos/list/_modal/codigo.tsx
+++ src/app/auth/productos/list/_modal/codigo.tsx
@@ -1,9 +1,9 @@
 import Icon from "@/components/ui/icon";
 import Barcode from "react-barcode";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useReactToPrint } from "react-to-print";
 import { ProductoResults } from "@/services/dto/productos/producto";
-import { Button, Dialog, DialogActions, DialogContent } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, Grid2 as Grid } from "@mui/material";
 import { useAuth } from "@/providers/AuthProvider";
 
 interface PrintCodigoModalProps {
@@ -16,6 +16,8 @@ function PrintCodigoModal({ open, onClose, selectedProducto }: PrintCodigoModalP
   if (!selectedProducto) return null;
 
   const { userData } = useAuth();
+  const [altura, setAltura] = useState(36);
+
   const contentRef = useRef<HTMLDivElement>(null);
   const print = useReactToPrint({ contentRef, ignoreGlobalStyles: true });
   const impresoraWidth = userData?.impresoras?.[0]?.mm ? `${userData.impresoras[0].mm}mm` : "100%";
@@ -23,22 +25,28 @@ function PrintCodigoModal({ open, onClose, selectedProducto }: PrintCodigoModalP
   return (
     <Dialog open={open} onClose={onClose} disableRestoreFocus>
       <DialogContent>
-        <div
-          ref={contentRef}
-          style={{
-            fontFamily: "monospace",
-            textAlign: "center",
-            border: "none",
-            color: "#000 !important",
-            width: impresoraWidth,
-            maxWidth: impresoraWidth,
-            marginTop: "10px",
-            background: "#fff",
-          }}
-        >
-          <Barcode value={selectedProducto.codigo} height={36} fontSize={14} font="monospace" background="#ffffff" margin={0} textMargin={0} format="CODE128" />
-          <span style={{ color: "#000", display: "block", fontFamily: "monospace", fontSize: 12 }}>{selectedProducto.precio_normal.toLocaleString("es-PY")}</span>
-        </div>
+        <Grid container spacing={2}>
+          <Grid size={12}>
+            <div
+              ref={contentRef}
+              style={{
+                fontFamily: "monospace",
+                textAlign: "center",
+                border: "none",
+                color: "#000 !important",
+                width: impresoraWidth,
+                maxWidth: impresoraWidth,
+                marginTop: "10px auto",
+                background: "#fff",
+                borderRadius: "4px",
+              }}
+            >
+              <Barcode value={selectedProducto.codigo} height={36} fontSize={14} font="monospace" background="#ffffff" margin={0} marginTop={2} textMargin={0} format="CODE128" />
+              <span style={{ color: "#000", display: "block", fontFamily: "monospace", fontSize: 12 }}>{selectedProducto.precio_normal.toLocaleString("es-PY")}</span>
+            </div>
+          </Grid>
+          <Grid size={12}></Grid>
+        </Grid>
       </DialogContent>
       <DialogActions>
         <Button startIcon={<Icon>printer</Icon>} color="primary" onClick={() => print()}>
