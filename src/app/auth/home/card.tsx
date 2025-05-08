0bfa3abdfb444483fc3acf14e3473896cd32e958
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-05-08 16:31:33 -0300

    ventas

--- src/app/auth/home/card.tsx
+++ src/app/auth/home/card.tsx
@@ -1,13 +1,17 @@
 import Icon from "@/components/ui/icon";
+import useThemeCustom from "@/hooks/useThemeCustom";
+import { colors } from "@/theme/colors";
 import { Box, Card, CardContent, Stack, Typography } from "@mui/material";
 
 interface CardHomeProps {
   data: number | string;
   title: string;
   icon?: string;
+  color?: string;
 }
 
-function CardHome({ data, title, icon }: CardHomeProps) {
+function CardHome({ data, title, icon, color }: CardHomeProps) {
+  const { customTheme } = useThemeCustom();
   return (
     <Card sx={{ boxShadow: 4 }}>
       <CardContent>
@@ -16,7 +20,11 @@ function CardHome({ data, title, icon }: CardHomeProps) {
             <Typography variant="h5">{data}</Typography>
             <Typography variant="caption">{title}</Typography>
           </Box>
-          {icon && <Icon size={36}>{icon}</Icon>}
+          {icon && (
+            <Icon color={color ?? customTheme?.palette.primary.main} size={36}>
+              {icon}
+            </Icon>
+          )}
         </Stack>
       </CardContent>
     </Card>
