808ff166a3e1f59cf12179eac7d12f4566c9b14b
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-05-13 13:36:39 -0300

    add: estado color

--- src/app/auth/ventas/pedidos/lista/_components/pedidosColumnConfig.tsx
+++ src/app/auth/ventas/pedidos/lista/_components/pedidosColumnConfig.tsx
@@ -2,15 +2,36 @@
 import { ColumnConfigType } from "@/core/types/columnconfig";
 import { TableCellProps } from "react-virtualized";
 import { format } from "@formkit/tempo";
-import { Stack, Tooltip, IconButton } from "@mui/material";
+import { Stack, Tooltip, IconButton, Chip, ChipOwnProps } from "@mui/material";
 import Icon from "@/components/ui/icon";
 import { PedidosDelDiaResults } from "@/services/dto/pedidos/pedidosDelDia";
 // import { useNavigate } from "react-router-dom"; // Si necesitas la navegación aquí
-const estados = {
-  "1": "Pendiente",
-  "2": "En Proceso",
-  "3": "Entregado",
-};
+
+const estadosKeys : Array<{
+  title: string,
+  color: ChipOwnProps['color']
+}> = [
+  {
+    title: 'Sin estado',
+    color: 'info'
+  },
+  {
+    title: 'Pendiente',
+    color: 'warning',
+  },
+  {
+    title: 'En proceso',
+    color: 'info',
+  },
+  {
+    title: 'Entregado',
+    color: 'success',
+  },
+  {
+    title: 'Cancelado',
+    color: 'error',
+  },
+];
 
 interface AccionesCellProps extends TableCellProps {
   rowData: PedidosDelDiaResults;
@@ -28,12 +49,12 @@ const AccionesCell = ({ rowData, onImprimir }: AccionesCellProps) => {
 
   return (
     <Stack direction="row">
-      <Tooltip title="Imprimir" placement="left" arrow>
+      <Tooltip title="Imprimir" placement="top" arrow>
         <IconButton onClick={() => onImprimir(rowData)}>
           <Icon>printer</Icon>
         </IconButton>
       </Tooltip>
-      <Tooltip title="Cancelar" placement="bottom" arrow>
+      <Tooltip title="Cancelar" placement="top" arrow>
         <IconButton onClick={() => console.log("Cancelar pedido ID:", rowData.id)}>
           <Icon>x</Icon>
         </IconButton>
@@ -43,29 +64,29 @@ const AccionesCell = ({ rowData, onImprimir }: AccionesCellProps) => {
 };
 
 export const pedidosColumnConfig = (width: number, onImprimir: (pedido: PedidosDelDiaResults) => void): ColumnConfigType[] => [
-  { dataKey: "id", label: "ID", width: width * 0.1 },
-  { dataKey: "doc", label: "Doc", width: width * 0.1 },
-  { dataKey: "razon_social", label: "Cliente", width: width * 0.2 },
+  { dataKey: "id", label: "ID", width: width * 0.07 },
+  {
+    dataKey: "created_at",
+    label: "Fecha",
+    width: width * 0.12,
+    cellRenderer: ({ rowData }: TableCellProps) => format(rowData.created_at, "DD-MM-YY HH:mm"),
+  },
+  { dataKey: "doc", label: "Doc", width: width * 0.12 },
+  { dataKey: "razon_social", label: "Cliente", width: width * 0.25 },
   {
     dataKey: "estado",
     label: "Estado",
     width: width * 0.1,
-    cellRenderer: ({ rowData }: TableCellProps) => {
-      const estado = rowData.estado as keyof typeof estados;
-      return estados[estado] || estado;
-    },
+    cellRenderer: ({ rowData }: TableCellProps) => <Chip color={estadosKeys[rowData.estado].color} size="small" label={estadosKeys[rowData.estado].title} />
+    ,
   },
-  { dataKey: "importe_final", label: "Total", width: width * 0.18, cellRenderer: ({ rowData }: TableCellProps) => rowData.importe_final.toLocaleString("es-PY") },
-  {
-    dataKey: "created_at",
-    label: "Fecha",
-    width: width * 0.18,
-    cellRenderer: ({ rowData }: TableCellProps) => format(rowData.created_at, "DD-MM-YY HH:mm"),
+  { dataKey: "importe_final", label: "Total", width: width * 0.1, 
+    cellRenderer: ({ rowData }: TableCellProps) => <p>{rowData.importe_final.toLocaleString("es-PY")}</p> 
   },
   {
     dataKey: "_",
     label: "Acciones",
-    width: width * 0.18,
+    width: width * 0.15,
     cellRenderer: (props: TableCellProps) => <AccionesCell {...props} onImprimir={onImprimir} />,
   },
 ];
