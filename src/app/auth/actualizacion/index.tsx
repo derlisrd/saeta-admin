1947ac628a309684a1360ccbb8dde08190a0c492
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-05-16 08:52:10 -0300

    refresh

--- src/app/auth/actualizacion/index.tsx
+++ src/app/auth/actualizacion/index.tsx
@@ -2,8 +2,15 @@ import Icon from "@/components/ui/icon";
 import { Button, Container } from "@mui/material";
 
 function Actualizacion() {
+
+
+    const refresh = () => {
+        localStorage.clear()
+        window.location.reload()
+    }
+
     return <Container>
-        <Button startIcon={<Icon>refresh</Icon>} onClick={()=> window.location.reload()}>Actualizar</Button>
+        <Button startIcon={<Icon>refresh</Icon>} onClick={() => refresh()}>Actualizar</Button>
     </Container>
 }
 
